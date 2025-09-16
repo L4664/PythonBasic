be923c526caa7b7e9811e34c609862164db848a2
L4664 <lars_mueller@sluz.ch>
2025-09-16 15:14:08 +0200

    Korregiert

--- task_1_1.py
+++ task_1_1.py
@@ -1,6 +1,8 @@
-
-name = input("Hey Du! Gib deinen Namen ein: ")
-
-print("Hallo" + name + "! Schon, dass du da bis.")
-
+# Name einlesen und begrüßen
+name = input("Wie heisst du? ")
 print(f"Hallo {name}! Schön, dass du da bist.")
+
+# Zwei Zahlen einlesen, Summe ausgeben
+zahl1 = float(input("Gib die erste Zahl ein: "))
+zahl2 = float(input("Gib die zweite Zahl ein: "))
+print(f"Die Summe ist: {zahl1 + zahl2}")
