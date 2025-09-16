e4411e85f1064296a7c78a85cfc70bd64271c1be
L4664 <lars_mueller@sluz.ch>
2025-09-16 15:34:54 +0200

    add answer

---
+++ task_1_8.py
@@ -0,0 +1,8 @@
+# Zahl liegt zwischen 10 und 20?
+zahl = float(input("Gib eine Zahl ein: "))
+if zahl >= 10 and zahl <= 20:
+    print(f"{zahl} liegt zwischen 10 und 20.")
+
+# Zahl kleiner als 0 oder grösser als 100?
+if zahl < 0 or zahl > 100:
+    print(f"{zahl} ist kleiner als 0 oder grösser als 100.")
