73550699226eefffceba3f715883aabf6755b19b
L4664 <lars_mueller@sluz.ch>
2025-09-16 15:20:55 +0200

    add task_1_3.py for name manipulation and word concatenation

---
+++ task_1_3.py
@@ -0,0 +1,11 @@
+# Name in verschiedenen Varianten ausgeben
+name = input("Wie heisst du? ")
+
+print(name.upper())       # Grossbuchstaben
+print(name.lower())       # Kleinbuchstaben
+print(name[::-1])         # Rückwärts
+
+# Zwei Wörter verbinden
+wort1 = input("Erstes Wort: ")
+wort2 = input("Zweites Wort: ")
+print("Neues Wort:", wort1 + wort2)
