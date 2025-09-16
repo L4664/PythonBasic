4489656b97fb1703da7ce90b444990a4d988cbe0
L4664 <lars_mueller@sluz.ch>
2025-09-16 15:21:12 +0200

    add new file

---
+++ task_1_4.py
@@ -0,0 +1,13 @@
+zahl1 = float(input("Zahl 1: "))
+zahl2 = float(input("Zahl 2: "))
+
+add = round(zahl1 + zahl2, 2)
+sub = round(zahl1 - zahl2, 2)
+mul = round(zahl1 * zahl2, 2)
+div = round(zahl1 / zahl2, 2)
+
+print(f"Addition: {add}")
+print(f"Subtraktion: {sub}")
+print(f"Multiplikation: {mul}")
+print(f"Division: {div}")
+
